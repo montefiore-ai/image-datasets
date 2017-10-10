ed7971a5b0c8be76b0c0b243d3b1796490ff51e8
Romain Mormont <romainmormont@hotmail.com>
2017-10-10 13:37:40 +0200

    first commit

---
+++ datasets/__init__.py
@@ -0,0 +1,1 @@
+from .load import ImageClassificationDataset, ImageSegmentationDataset, ImageProvider, ImageDataset
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="datasets",
+    version="1.0",
+    description="datasets, a set of utility classes for loading image datasets",
+    author="Romain Mormont",
+    author_email="romain.mormont@gmail.com",
+    packages=["datasets"]
+)
